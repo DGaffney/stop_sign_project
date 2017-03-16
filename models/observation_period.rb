694f053a896dfd1765ec1428c524ab72fe5aa38d
Devin Gaffney <itsme@devingaffney.com>
2017-03-16 15:04:18 -0400

    updates

--- models/observation_period.rb
+++ models/observation_period.rb
@@ -20,6 +20,6 @@ class ObservationPeriod
   end
   
   def logs
-    StopSignLog.where(observation_timestamp: self.observation_timestamp.utc.to_i).to_a
+    StopSignLog.where(observation_timestamp: self.observation_timestamp.utc.to_i)
   end
 end
